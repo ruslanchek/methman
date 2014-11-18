2940b555f991795b2f9ae856d7a2611b9cf4c3e9
Ruslan <rshashkov@icloud.com>
2014-11-18 15:45:51 +0300

    8-3 ok

--- collections/posts.js
+++ collections/posts.js
@@ -1,16 +1,16 @@
 Posts = new Mongo.Collection('posts');
 
 Posts.allow({
-    update: ownsDocument,
-    remove: ownsDocument
+  update: ownsDocument,
+  remove: ownsDocument
 });
 
 Posts.deny({
-    update: function(userId, post, fieldNames) {
-        return (_.without(fieldNames, 'url', 'title').length > 0);
-    }
+  update: function(userId, post, fieldNames) {
+    // разрешаем редактировать только следующие два поля:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
 });
-
 Meteor.methods({
     postInsert: function(postAttributes) {
         check(this.userId, String);
