6904dc731247d8683f6d93cc1d2593da251d9498
Ruslan <rshashkov@icloud.com>
2014-11-18 19:09:31 +0300

    Revert "9-1"
    This reverts commit 5028afdec4d20758485f02c44675a50265a67197.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -4,24 +4,21 @@ Template.postSubmit.events({
 
         var post = {
             url: $(e.target).find('[name=url]').val(),
-            title: $(e.target).find('[name=title]').val(),
-            message: $(e.target).find('[name=message]').val()
-        }
+            title: $(e.target).find('[name=title]').val()
+        };
 
-        Meteor.call('post', post, function(error, id) {
-            if (error) {
-                // показываем ошибку пользователю
-                throwError(error.reason);
+        Meteor.call('postInsert', post, function(error, result) {
+            // display the error to the user and abort
+            if (error)
+                return alert(error.reason);
 
-                if (error.error === 302)
-                    Router.go('postPage', {
-                        _id: error.details
-                    })
-            } else {
-                Router.go('postPage', {
-                    _id: id
-                });
-            }
+            // show this result but route anyway
+            if (result.postExists)
+                alert('This link has already been posted');
+
+            Router.go('postPage', {
+                _id: result._id
+            });
         });
     }
 });
